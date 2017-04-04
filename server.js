de5ea5a54bd3cbbee1417aef6a6a47719c948364
Arnar Freyr Helgason <nozagleh@gmail.com>
2017-04-04 17:09:42 +0200

    add todo, create new branch

--- server.js
+++ server.js
@@ -2,12 +2,16 @@ var express = require('express');
 var graphqlHTTP = require('express-graphql');
 var { buildSchema } = require('graphql');
 
+//TODO: remove the actual data from the server file and create a connection to a DB
+
 // Construct a schema, using GraphQL schema language
 var schema = buildSchema(`
   type Query {
     hello: String
   }
 `);
+
+//TODO: checkout sequelize
 
 // The root provides a resolver function for each API endpoint
 var root = {
